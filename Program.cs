c9816d6aa691b0e8fb042117ae55aa665a56e061
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-06-28 15:08:16 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿void StampaArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+
+    Console.WriteLine("]");
+}
+
+
+int NumeroQuadrato(int num)
+{
+    return num * num;
+}
+
+
+int[] ElevaArrayQuadrato(int[] array)
+{
+    int[] arrayQuadrato = new int[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayQuadrato[i] = NumeroQuadrato(array[i]);
+    }
+
+    return arrayQuadrato;
+}
+
+
+int SommaElementi(int[] array)
+{
+    int somma = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        somma += array[i];
+    }
+
+    return somma;
+}
+
+
+// Programma principale
+
+//int[] array = { 2, 6, 7, 5, 3, 9 }; // array fornito nella consegna
+
+Console.Write($"\nInserisci la lunghezza dell'array: ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[arrayLength];
+
+Console.WriteLine($"\nInserisci {arrayLength} numeri:");
+for (int i = 0; i < arrayLength; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("\nArray originale:");
+StampaArray(array);
+
+//Console.WriteLine(Quadrato(2));
+
+int[] arrayQuadrato = ElevaArrayQuadrato(array);
+
+Console.WriteLine("\nArray al quadrato:");
+StampaArray(arrayQuadrato);
+
+Console.WriteLine("\nArray originale:");
+StampaArray(array);
+
+Console.Write("\nSomma array originale: ");
+Console.WriteLine(SommaElementi(array));
+
+Console.Write("\nSomma array al quadrato: ");
+Console.WriteLine(SommaElementi(arrayQuadrato));
